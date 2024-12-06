8c815ccffe0847c334ec5231fbff68e063f35993
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-05 19:53:56 -0500

    Update Windows Update Fixer.bat

--- Windows Update Fixer.bat
+++ Windows Update Fixer.bat
@@ -2,7 +2,7 @@
 title Windows Update Fixer
 setlocal
 echo Program Name: Windows Update Fixer
-echo Version: 1.0.12
+echo Version: 1.0.13
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -163,7 +163,7 @@ echo Starting Windows Update services.
 net start bits > nul 2>&1
 net start wuauserv > nul 2>&1
 net start cryptsvc > nul 2>&1
-net start appidsvc> nul 2>&1
+net start appidsvc > nul 2>&1
 echo Windows Update services started.
 if /i "%Windows%"=="2" goto :"BITS"
 goto "Restart"
